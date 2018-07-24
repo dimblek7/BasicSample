cfe64256e5598dd011dbeb486686e838c87b5a78
Narayan Ratawa <41002657+narayan-ratawa@users.noreply.github.com>
2018-07-24 11:36:15 +0530

    Update home.js
    updated codacy issue related code

--- src/components/home.js
+++ src/components/home.js
@@ -1,25 +1,5 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-      'Double tap R on your keyboard to reload,\n' +
-      'Shake or press menu button for dev menu',
-  });
-  export default class Sample extends Component{
-render(){
-  return (
-    <View style={styles.container}>
-    <Text style={styles.welcome}>Welcome to React Native!</Text>
-    <Text style={styles.instructions}>To get started, edit App.js</Text>
-    <Text style={styles.instructions}>{this.props.title}</Text>
-  </View>
-);
-
-}
-    
-}
-
+import React, {Component} from "react";
+import {Platform, StyleSheet, Text, View} from "react-native";
 const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -38,3 +18,19 @@ const styles = StyleSheet.create({
       marginBottom: 5,
     },
   });
+
+  export default class Sample extends Component{
+render(){
+  return (
+    <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to React Native!</Text>
+    <Text style={styles.instructions}>To get started, edit App.js</Text>
+    <Text style={styles.instructions}>{this.props.title}</Text>
+  </View>
+);
+
+}
+    
+}
+
+
